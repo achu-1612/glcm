cf401676346d887622f80ce8d3f476c75442b45a
achu <achyuta.das@hpe.com>
2025-01-07 10:48:39 +0530

    example: unified service and hook

---
+++ example/advanced/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/achu-1612/glcm"
+	"github.com/achu-1612/glcm/hook"
+	svc "github.com/achu-1612/glcm/service"
+)
+
+/*
+    This example demonstrates how to use resutl of the hook execution in the service.
+	The ServiceA will implement the hook.Handler interface and the Service interface.
+	The prehook execution result will be stored in the struct which implements the service interface.
+*/
+
+var _ svc.Service = &ServiceA{}
+var _ hook.Handler = &ServiceA{}
+
+// ServiceA will imeplement the Service interface as well as the hook Handler interface.
+type ServiceA struct {
+	PreHookResult string
+}
+
+func (s *ServiceA) Start(ctx svc.Terminator) {
+	for {
+		<-time.After(time.Second * 2)
+
+		select {
+		case <-ctx.TermCh():
+			return
+
+		default:
+			log.Println("ServiceA is running ", time.Now())
+			log.Println("ServiceA PreHookResult: ", s.PreHookResult)
+		}
+	}
+}
+
+func (s *ServiceA) Status() string {
+	return ""
+}
+
+func (s *ServiceA) Name() string {
+	return "ServiceA"
+}
+
+func (s *ServiceA) Execute() error {
+	log.Println("ServiceA PreHook executed")
+	s.PreHookResult = "ServiceA PreHook executed successfully"
+
+	return nil
+}
+
+func main() {
+	base := glcm.NewRunner()
+
+	sA := &ServiceA{}
+
+	if err := base.RegisterService(
+		sA,
+		svc.WithPreHooks(
+			sA,
+		),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	base.BootUp(context.TODO())
+	base.Wait()
+}
